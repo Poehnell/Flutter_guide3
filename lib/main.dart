6b4434ae493b31fe6de72192cf5de55d9d6b2805
Ty <poehnell28@gmail.com>
2021-02-09 16:02:43 -0700

    Created a class QuoteList which is a stateful widget. created a class Quote wich is takes in 2 strings text and author. this is then used in the _QuoteListState.created 3 versions of Quote with authors and texts.Created a Widget quoteTemplate that returns a Card widget.In the Card widget will take the Quote text and author and stylize them into a card.Using the Flutter Outline tab we selected the Card and Extraced as widget. Naming it QuoteCard it then creats a statlessWidget of the card.Transfered QuoteCard to its own dart file.Created a constructor in QuoteCard to take in Quote as a property.Imported into main dart file.Created a List of Quotes and then called on them in the body using a map function to cycle through the list of Quotes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,87 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MaterialApp(
+      home: QuoteList(),
+    ));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class QuoteList extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(
+        author: 'Oscar Wilde ',
+        text: 'Be yourself; everyone else is already taken'),
+    Quote(
+        author: 'Oscar Wilde ',
+        text: 'I have nothing to declare except my genius'),
+    Quote(
+        author: 'Oscar Wilde ',
+        text: 'The truth is rarely pure and never simple')
+  ];
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  //USED ON SECOND BUILD
+  // Widget quoteTemplate(quote){
+  //   return QuoteCard(quote: quote);
+  // }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.red[900],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Column(
+        //FIST BUILD
+        // children: quotes.map((e) => Text('${e.text} - ${e.author}')).toList(),
+        //SECOND BUILD
+        // children: quotes.map((q) => quoteTemplate(q)).toList(),
+        //THIRD BUILD
+        children: quotes.map((q) => QuoteCard(quote: q)).toList(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+//USED IN SECOND BUILD WHEN UNCOMMENTED MADE THIS INTO ITS OWN DART FILE FOR THIRD BUILD
+// class QuoteCard extends StatelessWidget {
+//
+//   final Quote quote;
+//   QuoteCard({this.quote});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Card(
+//       margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+//       child: Padding(
+//         padding: const EdgeInsets.all(12.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.stretch,
+//           children: [
+//             Text(
+//               quote.text,
+//               style: TextStyle(
+//                   fontSize: 18.0,
+//                   color: Colors.grey[800]
+//               ),
+//             ),
+//             SizedBox(height: 6.0),
+//             Text(
+//               quote.author,
+//               style: TextStyle(
+//                   fontSize: 14.0,
+//                   color: Colors.grey[800]
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
